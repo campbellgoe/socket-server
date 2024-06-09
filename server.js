5a2692f0b07c8a56818244026031f0cc456e2276
George Campbell <campbell.goe@gmail.com>
2024-06-09 12:24:58 +0100

    latest code its not socket its just rest

--- server.js
+++ server.js
@@ -1,32 +1,49 @@
 const express = require('express');
-const http = require('http');
-const socketIo = require('socket.io');
+const util = require('node:util')
+const axios = require('axios');
 const cors = require('cors');
-
+require('dotenv').config();
+const port = process.env.PORT || 5000;
 const app = express();
 app.use(cors());
-const server = http.createServer(app);
-const io = socketIo(server, {
-  cors: {
-      origin: 'https://chat-frontend-tau-nine.vercel.app/', // Replace '*' with your frontend URL in production
-      methods: ['GET', 'POST']
+app.use(express.json());
+
+app.post('/api/chatgpt', async (req, res) => {
+  try {
+    const modelsData = await axios.get('https://api.openai.com/v1/models', {
+                headers: {
+                    'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`,
+                },
+            })
+    const models = modelsData.data.data
+    const modelId = 'gpt-4o'
+    const gpt4oExists = models.find(({ id }) => id === modelId)
+    if(gpt4oExists){
+     const response = await axios.post(
+          `https://api.openai.com/v1/chat/completions`,
+          {
+            model: modelId,
+            messages: [{"role": "user", "content": req.body.prompt }],
+          },
+          {
+              headers: {
+                  'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`,
+              },
+          }
+      );
+      res.json({
+        message: response.data.choices[0].message.content
+      });
+    } else {
+      res.json({ error: 'gpt-4o model wasnt\'t found.'})
+    }
+ 
+  } catch (error) {
+      console.error(util.inspect(error, true, 6));
+      res.status(500).send('Error querying OpenAI API');
   }
 });
 
-io.on('connection', (socket) => {
-    console.log('A user connected');
-    
-    socket.on('chat_message', (msg) => {
-      console.log('chat message received by server:', msg)
-        io.emit('chat_message', msg);
-    });
-
-    socket.on('disconnect', () => {
-        console.log('User disconnected');
-    });
-});
-
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
 });
